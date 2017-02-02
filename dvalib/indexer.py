23a7ed0b8e6144329c7e4e1e26ffc777773d992f
akshay bhat <akshayubhat@gmail.com>
2017-02-02 04:48:53 -0500

    added very rudimentary implementation of darknet yolo 9000 detection, also fixed a bug in query results.

--- dvalib/indexer.py
+++ dvalib/indexer.py
@@ -111,7 +111,7 @@ class AlexnetIndexer(BaseIndexer):
             logging.warning("Loading the network")
             self.net = alexnet(pretrained=True)
             self.transform = transforms.Compose([
-                transforms.Scale(224),
+                transforms.RandomCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
             ])

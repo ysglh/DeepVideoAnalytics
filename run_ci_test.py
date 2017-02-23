48a3b22738759be2e578912da5e28d2f166a79fd
akshay bhat <akshayubhat@gmail.com>
2017-02-23 03:06:25 -0500

    youtube-dl integration tests

--- run_ci_test.py
+++ run_ci_test.py
@@ -5,7 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dva.settings")
     django.setup()
-    from dvaapp.views import handle_uploaded_file
+    from dvaapp.views import handle_uploaded_file,handle_youtube_video
     from dvaapp.models import Video
     from dvaapp.tasks import extract_frames,perform_indexing
     for fname in glob.glob('tests/ci/*.mp4'):
@@ -16,6 +16,7 @@ if __name__ == '__main__':
         name = fname.split('/')[-1].split('.')[0]
         f = SimpleUploadedFile(fname, file(fname).read(), content_type="application/zip")
         handle_uploaded_file(f, name)
+    handle_youtube_video('yolov2', 'https://www.youtube.com/watch?v=VOC3huqHrss')
     for v in Video.objects.all():
         extract_frames(v.pk)
     for v in Video.objects.all():

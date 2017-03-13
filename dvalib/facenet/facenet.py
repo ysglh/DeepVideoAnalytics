7c6019bf88effb4cd7f963556423a264bc4ee1d7
akshay bhat <akshayubhat@gmail.com>
2017-03-13 01:51:26 -0400

    explicitly using PIL instead of scipy.misc.imread

--- dvalib/facenet/facenet.py
+++ dvalib/facenet/facenet.py
@@ -38,6 +38,7 @@ from tensorflow.python.framework import ops
 from tensorflow.python.training import training
 import random
 import re
+import PIL
 
 
 def triplet_loss(anchor, positive, negative, alpha):
@@ -255,7 +256,7 @@ def load_data(image_paths, do_random_crop, do_random_flip, image_size, do_prewhi
     nrof_samples = len(image_paths)
     images = np.zeros((nrof_samples, image_size, image_size, 3))
     for i in range(nrof_samples):
-        img = misc.imread(image_paths[i],mode='RGB')
+        img = PIL.Image.open(image_paths[i]).convert('RGB')
         if img.ndim == 2:
             img = to_rgb(img)
         if do_prewhiten:

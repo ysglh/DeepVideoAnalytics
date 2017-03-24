921ad94a8bfc6b41bd6843814819c982b62e3d0c
akshay bhat <akshayubhat@gmail.com>
2017-03-24 05:08:48 -0400

    new AMI

--- experiments/config.py
+++ experiments/config.py
@@ -1,4 +1,4 @@
-AMI = 'ami-b3cc1fa5'  # AMI
+AMI = 'ami-b9992baf'  # AMI
 KeyName = 'C:\\fakepath\\cs5356'  # REPLACE WITH YOUR OWN
 SecurityGroupId = 'sg-06a6b562'  # REPLACE WITH YOUR OWN
 IAM_ROLE = {'Arn': 'arn:aws:iam::248089713624:instance-profile/chdeploy',}   # REPLACE WITH YOUR OWN (Provide S3 access to allow backups)

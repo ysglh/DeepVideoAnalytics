ae7f7c2c5b5c5fd14535810f82f74f1799c55782
akshay bhat <akshayubhat@gmail.com>
2017-04-05 18:21:11 -0400

    refactored imports into serializer, allow multiple imports

--- dvaapp/serializers.py
+++ dvaapp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, viewsets
 from django.contrib.auth.models import User
 from models import Video, VLabel, Frame, Annotation, Detection, Query, QueryResults, TEvent, IndexEntries
+import os,json,logging
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -112,3 +113,118 @@ class VideoExportSerializer(serializers.ModelSerializer):
                   'frames','created','description','uploaded','dataset',
                   'uploader','detections','url','youtube_video','annotation_list',
                   'frame_list','detection_list','index_entries_list')
+
+def import_frame(f,video_obj):
+    df = Frame()
+    df.video = video_obj
+    df.name = f['name']
+    df.frame_index = f['frame_index']
+    df.subdir = f['subdir']
+    df.save()
+    return df
+
+def import_detection(d,video_obj,frame_to_pk,video_root_dir):
+    dd = Detection()
+    dd.video = video_obj
+    dd.x = d['x']
+    dd.y = d['y']
+    dd.h = d['h']
+    dd.w = d['w']
+    dd.frame_id = frame_to_pk[d['frame']]
+    dd.confidence = d['confidence']
+    dd.object_name = d['object_name']
+    dd.metadata = d['metadata']
+    dd.save()
+    orginal = '{}/detections/{}.jpg'.format(video_root_dir, d['id'])
+    temp_file = "{}/detections/d_{}.jpg".format(video_root_dir, dd.pk)
+    converted = "{}/detections/{}.jpg".format(video_root_dir, dd.pk)
+    os.rename(orginal, temp_file)
+    os.rename(temp_file, converted)
+    return dd
+
+
+def import_annotation(a,video_obj,frame_to_pk,detection_to_pk):
+    da = Annotation()
+    da.video = video_obj
+    da.x = a['x']
+    da.y = a['y']
+    da.h = a['h']
+    da.w = a['w']
+    if a['label'].strip():
+        da.label = a['label']
+        label_object, created = VLabel.objects.get_or_create(label_name=a['label'], source=VLabel.UI)
+        da.label_parent = label_object
+    da.frame_id = frame_to_pk[a['frame']]
+    if a['detection']:
+        da.detection_id = detection_to_pk[a['detection']]
+    da.full_frame = a['full_frame']
+    da.metadata_text = a['metadata_text']
+    da.save()
+    return da
+
+
+def import_index_entries(i,video_obj,previous_transformed,detection_to_pk,frame_to_pk,video_root_dir):
+    di = IndexEntries()
+    di.video = video_obj
+    di.algorithm = i['algorithm']
+    di.count = i['count']
+    di.contains_detections = i['contains_detections']
+    di.contains_frames = i['contains_frames']
+    di.approximate = i['approximate']
+    di.created = i['created']
+    di.features_file_name = i['features_file_name']
+    di.entries_file_name = i['entries_file_name']
+    di.detection_name = i['detection_name']
+    signature = "{}".format(di.entries_file_name)
+    if signature in previous_transformed:
+        logging.warning("repeated index entries found, skipping {}".format(signature))
+    else:
+        previous_transformed.add(signature)
+        transform_index_entries(di, detection_to_pk, frame_to_pk, video_obj.pk, video_root_dir)
+        di.save()
+
+def transform_index_entries(di,detection_to_pk,frame_to_pk,video_id,video_root_dir):
+    entries = json.load(file('{}/indexes/{}'.format(video_root_dir, di.entries_file_name)))
+    transformed = []
+    for entry in entries:
+        entry['video_primary_key'] = video_id
+        if 'detection_primary_key' in entry:
+            entry['detection_primary_key'] = detection_to_pk[entry['detection_primary_key']]
+        if 'frame_primary_key' in entry:
+            entry['frame_primary_key'] = frame_to_pk[entry['frame_primary_key']]
+        transformed.append(entry)
+    with open('{}/indexes/{}'.format(video_root_dir, di.entries_file_name),'w') as output:
+        json.dump(transformed,output)
+
+
+def import_video_json(video_obj,video_json,video_root_dir,old_key):
+    video_obj.name = 'Imported:'+ video_json['name']
+    video_obj.frames = video_json['frames']
+    video_obj.height = video_json['height']
+    video_obj.width = video_json['width']
+    video_obj.detections = video_json['detections']
+    video_obj.youtube_video = video_json['youtube_video']
+    video_obj.dataset = video_json['dataset']
+    video_obj.url = video_json['url']
+    video_obj.description = video_json['description']
+    video_obj.metadata = video_json['metadata']
+    video_obj.length_in_seconds = video_json['length_in_seconds']
+    video_obj.save()
+    if not video_obj.dataset:
+        old_video_path = "{}/video/{}.mp4".format(video_root_dir,old_key)
+        new_video_path = "{}/video/{}.mp4".format(video_root_dir,video_obj.pk)
+        os.rename(old_video_path,new_video_path)
+    frame_to_pk = {}
+    detection_to_pk = {}
+    for f in video_json['frame_list']:
+        df = import_frame(f,video_obj)
+        frame_to_pk[f['id']] = df.pk
+    for d in video_json['detection_list']:
+        dd = import_detection(d,video_obj,frame_to_pk,video_root_dir)
+        detection_to_pk[d['id']]=dd.pk
+    for a in video_json['annotation_list']:
+        da = import_annotation(a,video_obj,frame_to_pk,detection_to_pk)
+    previous_transformed = set()
+    for i in video_json['index_entries_list']:
+        import_index_entries(i, video_obj, previous_transformed, detection_to_pk, frame_to_pk, video_root_dir)
+    os.remove("{}/{}.zip".format(video_root_dir, video_obj.pk))

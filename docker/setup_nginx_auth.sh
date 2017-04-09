970c341a1d3c1e2b384fd9fe3efe0717113ffc04
akshay bhat <akshayubhat@gmail.com>
2017-04-09 19:17:43 -0400

    [skip ci] setup nginx basic auth now works!

--- docker/setup_nginx_auth.sh
+++ docker/setup_nginx_auth.sh
@@ -3,4 +3,4 @@ rm .htpasswd
 echo -n 'dvauser:' >> .htpasswd
 openssl passwd -apr1 >> .htpasswd
 docker cp .htpasswd dva-server:/etc/nginx/.htpasswd
-docker exec -u="root" -it dva-server "service nginx restart"
+docker exec -u="root" -it dva-server bash -c "supervisorctl restart nginx-app"

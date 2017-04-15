61b51b7f192a92fb2a8da1d0c3c12c285cbebb58
akshay bhat <akshayubhat@gmail.com>
2017-04-15 15:28:44 -0400

    copy scoped aws creds to container after restart.  [skip ci]

--- docker/restart_linode.sh
+++ docker/restart_linode.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose-linode.yml down 
-docker-compose -f docker-compose-linode.yml up
+docker-compose -f docker-compose-linode.yml up -d
+sleep 30
+./copy.sh

a903e817f38a79505768a9858e0d45991dea3707
akshay bhat <akshayubhat@gmail.com>
2017-04-24 08:53:49 -0400

    AWS requester pays s3 buckets finally work after changing the policy for both GetObject as well as ListObject.  Verified with a seperate AWS account. [skip ci]

--- docker/clean_restart_linode.sh
+++ docker/clean_restart_linode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose-linode.yml down -v
-docker-compose -f docker-compose-linode.yml up
+docker-compose -f docker-compose-linode.yml up -d
 sleep 120
 docker exec -u="root" -it dva-server bash -c "pip install --upgrade awscli"
 docker cp ~/.aws dva-server:/root/.aws

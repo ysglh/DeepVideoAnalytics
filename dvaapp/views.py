878d0ec0070b63793d584a639184abfbe2269158
akshay bhat <akshayubhat@gmail.com>
2017-05-04 06:47:03 -0400

    Improving UI adn bugfix

--- dvaapp/views.py
+++ dvaapp/views.py
@@ -194,6 +194,7 @@ def index(request,query_pk=None,frame_pk=None,detection_pk=None):
     context['pending_tasks'] = TEvent.objects.all().filter(started=False).count()
     context['running_tasks'] = TEvent.objects.all().filter(started=True,completed=False).count()
     context['successful_tasks'] = TEvent.objects.all().filter(started=True,completed=True).count()
+    context['errored_tasks'] = TEvent.objects.all().filter(errored=True).count()
     context['video_count'] = Video.objects.count() - context['query_count']
     context['index_entries'] = IndexEntries.objects.all()
     context['detection_count'] = Region.objects.all().filter(region_type=Region.DETECTION).count()

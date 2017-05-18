9ceb7e65f6756b4b680586167704ad227e2cc411
akshay bhat <akshayubhat@gmail.com>
2017-05-18 16:43:21 -0400

    Optional integration with DVA private repo used for research / development. modify buildpack-run.sh.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 set -xe
-mkdir dvap && touch test.txt
+aws s3 cp s3://aub3config/.netrc .netrc # using AWS credentials stored in environment variables copy .netrc from private bucket
+git clone https://github.com/AKSHAYUBHAT/DeepVideoAnalyticsDemo # clone private repo
+mv DeepVideoAnalyticsDemo dvap # mv cloned repo
+rm .netrc # remove .netrc

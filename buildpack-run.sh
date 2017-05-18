6e6b6e3f612427c873c785c5a67f3386f3521e08
akshay bhat <akshayubhat@gmail.com>
2017-05-18 17:05:58 -0400

    Optional integration with DVA private repo used for research / development. modify buildpack-run.sh.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -xe
-mkdir ~/.aws
-
-cat >> ~/.aws/credentials << EOF
-[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
-EOF
-cat ~/.aws/credentials
+export_env_dir() {
+  env_dir=$ENV_DIR
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+export_env_dir
 aws s3 cp s3://aub3config/.netrc ~/.netrc # using AWS credentials stored in environment variables copy .netrc from private bucket
 git clone https://github.com/AKSHAYUBHAT/DeepVideoAnalyticsDemo # clone private repo
 mv DeepVideoAnalyticsDemo dvap # mv cloned repo

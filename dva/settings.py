11db98f7b8b36fa8bdd92644529c1bccd03e9c6b
akshay bhat <akshayubhat@gmail.com>
2017-05-18 16:12:27 -0400

    Optional integration with DVA private repo used for research / development.

--- dva/settings.py
+++ dva/settings.py
@@ -14,7 +14,7 @@ import os,dj_database_url,sys
 from .worker_config import *
 
 VDN_ENABLE = 'VDN_ENABLE' in os.environ
-
+DVA_PRIVATE_ENABLE = 'DVA_PRIVATE_ENABLE' in os.environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -70,7 +70,8 @@ INSTALLED_APPS = [
     'vdnapp',
     'crispy_forms',
     'rest_framework.authtoken'
-]
+] + (['dvap',] if DVA_PRIVATE_ENABLE else [])
+
 
 if VDN_ENABLE:
     MIDDLEWARE_CLASSES = [

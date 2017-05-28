a63075095ff0180a7a0bfd1378e80b1acb51099c
akshay bhat <akshayubhat@gmail.com>
2017-05-28 14:02:33 -0400

    simplifying docker compose  by moving them into a seperate folder [skip ci]

--- docker/clean_restart_linode.sh
+++ docker/clean_restart_linode.sh
@@ -2,6 +2,7 @@
 docker-compose -f custom_compose/docker-compose-linode.yml down -v
 docker-compose -f custom_compose/docker-compose-linode.yml up -d
 sleep 120
+set -xe
 docker exec -u="root" -it dva-server bash -c "fab superu"
 docker exec -u="root" -it dva-server bash -c "pip install --upgrade awscli"
 docker cp ~/.aws dva-server:/root/.aws

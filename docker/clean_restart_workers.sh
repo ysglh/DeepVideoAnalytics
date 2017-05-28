afd032904193fc83d7451599d16a49d9a869983f
akshay bhat <akshayubhat@gmail.com>
2017-05-28 18:58:14 -0400

    Removed unecessary yml files.

--- docker/clean_restart_workers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source heroku.env
-docker-compose -f custom_compose/docker-compose-worker.yml down -v
-docker-compose -f custom_compose/docker-compose-worker.yml up -d
-sleep 120
-docker exec -u="root" -it dva-server bash -c "fab superu"
-docker exec -u="root" -it dva-server bash -c "pip install --upgrade awscli"
-docker cp ~/.aws dva-server:/root/.aws

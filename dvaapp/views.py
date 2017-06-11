d61b694607f34187d3b2bbee571f4d1fa41fe073
akshay bhat <akshayubhat@gmail.com>
2017-06-11 07:26:12 -0400

    Bug when class distribution is empty

--- dvaapp/views.py
+++ dvaapp/views.py
@@ -225,7 +225,8 @@ class DetectionDetail(UserPassesTestMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DetectionDetail, self).get_context_data(**kwargs)
-        context['class_distribution'] = json.loads(context['object'].class_distribution)
+        classdist = context['object'].class_distribution.strip()
+        context['class_distribution'] = json.loads(classdist) if classdist else {}
         return context
 
     def test_func(self):

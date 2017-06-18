6d82fc55bd5c8bdf35a8a75029b75fb2a3c7033d
akshay bhat <akshayubhat@gmail.com>
2017-06-18 02:41:55 -0400

    CUDA 8.0 version of CTPN [skip ci]

--- docker/ctpn/run_ocr_container.sh
+++ docker/ctpn/run_ocr_container.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nvidia-docker run -p 8888:8888 --name ctpn -it akshayubhat/dva_ctpn
+nvidia-docker run -p 8888:8888 --name ctpn -it akshayubhat/dva_ctpn /bin/bash -c "git pull && jupyter notebook --no-browser --allow-root"

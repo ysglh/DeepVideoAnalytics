7a98650b12bd3569cb9317c6407d749c065f50d9
akshay bhat <akshayubhat@gmail.com>
2017-07-03 23:06:18 -0400

    Default indexers and simplifying migrations

--- dvaapp/migrations/0011_textsearch_indexes.py
+++ dvaapp/migrations/0011_textsearch_indexes.py
@@ -8,7 +8,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dvaapp', '0010_indexentries_indexer'),
     ]
 
     operations = [
--- dvaapp/migrations/0012_default_indexers.py
+++ dvaapp/migrations/0012_default_indexers.py
@@ -5,11 +5,26 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
+def forwards_func(apps, schema_editor):
+    CustomIndexer = apps.get_model("dvaapp", "CustomIndexer")
+    db_alias = schema_editor.connection.alias
+    CustomIndexer.objects.using(db_alias).bulk_create([
+        CustomIndexer(name="inception",algorithm="inception"),
+        CustomIndexer(name="facenet",algorithm="facenet"),
+    ])
+
+
+def reverse_func(apps, schema_editor):
+    CustomIndexer = apps.get_model("dvaapp", "CustomIndexer")
+    db_alias = schema_editor.connection.alias
+    CustomIndexer.objects.using(db_alias).filter(name="inception").delete()
+    CustomIndexer.objects.using(db_alias).filter(name="facenet").delete()
+
+
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('dvaapp', '0011_textsearch_indexes'),
-    ]
+    dependencies = []
 
     operations = [
+        migrations.RunPython(forwards_func, reverse_func),
     ]

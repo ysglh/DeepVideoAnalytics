3f02fb75eda0def3e2a25a0d831f48373c532af6
akshay bhat <akshayubhat@gmail.com>
2017-08-08 01:10:36 -0400

    Add constraint on Label. Use get or create correctly.

--- dvaapp/operations/decoding.py
+++ dvaapp/operations/decoding.py
@@ -234,7 +234,7 @@ class VideoDecoder(object):
                         labels_to_frame[l].add((df_ids[i].id,f.frame_index))
         label_list = []
         for l in labels_to_frame:
-            dl = Label.objects.get_or_create(name=l,set="Directory")
+            dl, _ = Label.objects.get_or_create(name=l,set="Directory")
             for fpk,frame_index in labels_to_frame[l]:
                 a = FrameLabel()
                 a.video_id = self.dvideo.pk

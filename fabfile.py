42148cc69de7ec25f0eb8f425dded6064bfa14ef
akshay bhat <akshayubhat@gmail.com>
2017-08-15 20:09:35 -0400

    Export UI fixed. Next make custom object detector in-memory, and handle memory allocation via implicit queue management.

--- fabfile.py
+++ fabfile.py
@@ -435,7 +435,8 @@ def test():
     django.setup()
     from django.core.files.uploadedfile import SimpleUploadedFile
     from dvaapp.views import handle_uploaded_file, handle_video_url
-    local("fab submit:tests/scripts/url.json")
+    for fname in glob.glob('tests/scripts/*.json'):
+        local("fab submit:{}".format(fname))
     for fname in glob.glob('tests/*.mp4'):
         name = fname.split('/')[-1].split('.')[0]
         f = SimpleUploadedFile(fname, file(fname).read(), content_type="video/mp4")

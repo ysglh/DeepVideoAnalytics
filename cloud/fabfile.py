a8f78bf1e50d35f2e64fb0ddd08ad75bd57d4816
akshay bhat <akshayubhat@gmail.com>
2017-09-29 15:38:22 -0400

    Switch to cloud config.
    Next:
    
    - Reduce use of dvaapp in dvaui. Eventually ensuring that everything goes through the REST API.
    - Store uploaded videos/zip files to "ingest"  directory and use create_launch functionality to separate interface cleanly and ensuring everything goes through DVAPQL.
    - improve paper & documentation

--- cloud/fabfile.py
+++ cloud/fabfile.py
@@ -45,7 +45,9 @@ def launch(gpu_count=1,cpu_count=0):
                        "AvailabilityZone":"us-east-1a,us-east-1b,us-east-1c,us-east-1d,us-east-1e,us-east-1f"
                    },
                    IamInstanceProfile=IAM_ROLE)
-    user_data_cpu = file('initdata/worker_cpu.sh').read().format(EFS_DNS,CONFIG_BUCKET,"docker-compose-worker-cpu.yml")
+    user_data_cpu = file('initdata/cpu.yml').read().format(EFS_DNS,
+                                                       CONFIG_BUCKET,
+                                                       base64.b64encode(file("compose/docker-compose-worker-cpu.yml").read()))
     ec2spec_cpu = dict(ImageId=AMI,
                    KeyName=KeyName,
                    SecurityGroups=[{'GroupId': SecurityGroupId},],

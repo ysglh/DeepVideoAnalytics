2dec2d7348358222326304a2984f67284b57b57e
akshay bhat <akshayubhat@gmail.com>
2017-10-03 02:01:06 -0400

    Automating GPU AMI creation using AWS Deep Learning AMI.
    Next:
    
    - Reduce use of dvaapp in dvaui. Eventually ensuring that everything goes through the REST API.
    - Store uploaded videos/zip files to "ingest"  directory and use create_launch functionality to separate interface cleanly and ensuring everything goes through DVAPQL.
    - improve paper & documentation

--- cloud/gpu_ami.sh
+++ cloud/gpu_ami.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 # http://docs.aws.amazon.com/batch/latest/userguide/batch-gpu-ami.html
 # http://docs.aws.amazon.com/batch/latest/userguide/example-job-definitions.html#example-test-gpu
 # Install ecs-init, start docker, and install nvidia-docker
@@ -10,6 +11,7 @@ sudo rpm -ivh --nodeps nvidia-docker-1.0.1-1.x86_64.rpm
 # Validate installation
 rpm -ql nvidia-docker
 rm nvidia-docker-1.0.1-1.x86_64.rpm
+sudo usermod -aG docker $(whoami)
 
 # Make sure the NVIDIA kernel modules and driver files are bootstraped
 # Otherwise running a GPU job inside a container will fail with "cuda: unknown exception"
@@ -21,8 +23,8 @@ sudo /var/lib/cloud/scripts/per-boot/00_nvidia-modprobe
 # Start the nvidia-docker-plugin and run a container with
 # nvidia-docker (retry up to 4 times if it fails initially)
 sudo -b nohup nvidia-docker-plugin > /tmp/nvidia-docker.log
-sudo docker pull nvidia/cuda
-COMMAND="sudo nvidia-docker run nvidia/cuda nvidia-smi"
+sudo docker pull nvidia/cuda:8.0
+COMMAND="sudo nvidia-docker run nvidia/cuda:8.0 nvidia-smi"
 for i in {1..5}; do $COMMAND && break || sleep 15; done
 
 # Create symlink to latest nvidia-driver version

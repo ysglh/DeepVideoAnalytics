4e5a288461b083430d868a87212597ea598ee083
akshay bhat <akshayubhat@gmail.com>
2017-10-21 23:27:47 -0700

    Frames and Segments should have event id for tracking provenance. Sync now uses boto API and syncs only those files which were modified by the task. This enables synchronized uploads to remote S3 buckets opening up implementation that does not requires shared file system. Add media bucket to Linode to test sync.
    Next:
    - Reduce use of dvaapp in dvaui. Eventually ensuring that everything goes through the REST API.
    - Store uploaded videos/zip files to "ingest"  directory and use create_launch functionality to separate interface cleanly and ensuring everything goes through DVAPQL.
    - improve paper & documentation

--- deploy/single/clean_restart.sh
+++ deploy/single/clean_restart.sh
@@ -2,7 +2,9 @@
 docker-compose -f compose/docker-compose-linode.yml down -v
 set -xe
 docker-compose -f compose/docker-compose-linode.yml up -d
-sleep 120
+sleep 20
+docker cp ~/.aws dva-server:/root/.aws
+docker cp ~/.aws dva-caffe:/root/.aws
+sleep 100
 docker exec -u="root" -it dva-server bash -c "fab superu"
 docker exec -u="root" -it dva-server bash -c "pip install --upgrade awscli"
-docker cp ~/.aws dva-server:/root/.aws
--- deploy/single/restart.sh
+++ deploy/single/restart.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose-linode.yml down
 docker-compose -f docker-compose-linode.yml up -d
-sleep 120
+sleep 20
+docker cp ~/.aws dva-server:/root/.aws
+docker cp ~/.aws dva-caffe:/root/.aws
+sleep 100
 docker exec -u="root" -it dva-server bash -c "fab superu"
 docker exec -u="root" -it dva-server bash -c "pip install --upgrade awscli"
-docker cp ~/.aws dva-server:/root/.aws

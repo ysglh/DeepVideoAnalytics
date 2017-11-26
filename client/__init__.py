02b3f8cdf7e5c633bd42006c3e8e4959b558a7d8
akshay bhat <akshayubhat@gmail.com>
2017-11-25 18:20:56 -0800

    Added skeleton of  Client REST API wrapper to enable creation of retrieval experiments, tests and benchmarking scripts.
    Ensure text search on labels work.  Add UI for viewing labeled objects such as frames, segments, regions, videos, etc.
    
    Working on ability to import region annotations specified in JSON file with filenames and frame_index used as invariant identifiers.
    Streamlining creation of pre-indexed datasets for LFW, COCO and YFCC subsets. Also add ability to import from a local dir /ingest.
    
    Next:
    1. Reduce use of dvaapp in dvaui. Eventually ensuring that everything goes through the REST API.
    2. improve paper & documentation

--- client/__init__.py
+++ client/__init__.py
@@ -1,21 +1,47 @@
 """
 A simple wrapper around Deep Video Analytics REST API
 """
+import os, json, requests
 
 
 class DVAContext(object):
 
     def __init__(self,server=None,token=None):
-        pass
+        if server:
+            self.server = "{server}/api/".format(server=server)
+        else:
+            self.server = 'http://localhost:8000/api/'
+        if token:
+            self.token = token
+        elif 'DVA_TOKEN' in os.environ:
+            self.token = os.environ['DVA_TOKEN']
+        elif os.path.isfile('creds.json'):
+            self.token = json.loads(file('creds.json').read())['token']
+        self.headers = {'Authorization': 'Token {}'.format(self.token)}
 
     def list_videos(self):
-        pass
+        r = requests.get("{server}/videos/".format(server=self.server),
+                      headers=self.headers)
+        if r.ok:
+            return r.json()
+        else:
+            r.raise_for_status()
 
     def list_queries(self):
-        pass
+        r = requests.get("{server}/queries/".format(server=self.server),
+                      headers=self.headers)
+        if r.ok:
+            return r.json()
+        else:
+            r.raise_for_status()
 
     def list_models(self):
-        pass
+        r = requests.get("{server}/models/".format(server=self.server),
+                      headers=self.headers)
+        if r.ok:
+            return r.json()
+        else:
+            r.raise_for_status()
 
     def list_events(self):
         pass

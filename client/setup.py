3399e9671b4272108d212f680d35f39d2ef867c8
akshay bhat <akshayubhat@gmail.com>
2017-11-27 23:42:40 -0800

    Convert DVA client into a standalone python library.
    Add tutorial using client API. Improve documentation, split cloud into AWS/Heroku and GCP with Kubernetes example.
    Next create an intermediate tutorial using COCO dataset. Leverage ability to import region annotations specified in
    JSON file with filenames and frame_index used as invariant identifiers.  Streamlining creation of pre-indexed datasets for
    LFW, COCO and YFCC subsets. Also add ability to import from a local dir /ingest.
    
    Next:
    1. Reduce use of dvaapp in dvaui. Eventually ensuring that everything goes through the REST API.
    2. improve paper & documentation

---
+++ client/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DeepVideoAnalyticsClient',
+      version='1.0',
+      description='Deep Video Analytics Client',
+      author='Akshay Bhat',
+      author_email='dvaclient@deepvideoanalytics.com',
+      url='https://www.deepvideoanalytics.com/',
+      packages=['dvaclient'],
+     )

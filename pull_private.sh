6a8e64d9b28fd1bda0be12a68ab568c56cacf778
akshay bhat <akshayubhat@gmail.com>
2017-12-04 22:33:55 -0800

    Add ability to pull from private remote repo instead of public repo.
    Next:
    
    0. Add ability to import external datasets as list of URLs/S3/GS paths along with annotations.
    
    1. Add tutorial using client API. Improve documentation
    
    2. Leverage ability to import region annotations specified in  JSON file with filenames and frame_index used as invariant identifiers.
    
    3. Create an intermediate tutorial using COCO dataset Streamlining creation of pre-indexed datasets for LFW, COCO and YFCC subsets.
    
    4. Add ability to import from a local ingest directory inside media volume (careful, might lead to attackers being able read any file on FS).
    
    5.  Move remaining delete, rename and label logic to go through the REST API.
    
    6. Finish paper documenting the architecture.

--- pull_private.sh
+++ pull_private.sh
@@ -4,3 +4,4 @@ aws s3 cp s3://aub3config/.netrc ~/.netrc
 # Add remote and pull repo
 git remote add private https://github.com/AKSHAYUBHAT/DeepVideoAnalyticsPrivate
 git pull private master
+rm ~/.netrc

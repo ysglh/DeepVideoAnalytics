da5daf8bdb20b61877ad6eb1b55ef88fb3ef0856
akshay bhat <akshayubhat@gmail.com>
2017-12-17 17:54:46 -0800

    Testing on AWS GPU AMI. Installation instructions with latest AWS GPU AMI for P2 instances and NVidia Docker 2.0.
    Next:
    
    0. Soon label import support
    
    1. Add JSONSchema validation for DVAPQL scripts. Move remaining delete, rename and label logic to go through the REST API.
    
    2. Finish paper documenting the architecture.

--- deploy/single/install_nvidia_docker.sh
+++ deploy/single/install_nvidia_docker.sh
@@ -6,4 +6,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docke
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
-docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
+docker run --runtime=nvidia --rm nvidia/cuda:8.0 nvidia-smi
+#https://github.com/NVIDIA/nvidia-docker/issues/568
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose

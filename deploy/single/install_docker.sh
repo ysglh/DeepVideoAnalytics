01fb9bffba20d3db1c9c8bbe86eb0881ef099145
akshay bhat <akshayubhat@gmail.com>
2017-12-17 17:28:44 -0800

    Testing on AWS GPU AMI.
    Next:
    
    0. Soon label import support
    
    1. Add JSONSchema validation for DVAPQL scripts. Move remaining delete, rename and label logic to go through the REST API.
    
    2. Finish paper documenting the architecture.

---
+++ deploy/single/install_docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce=17.09.0~ce-0~ubuntu
+sudo usermod -aG docker ubuntu
+exit

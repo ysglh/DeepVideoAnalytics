84abe089f7dfcd702a2bda67910473965566729a
akshay bhat <akshayubhat@gmail.com>
2017-12-25 13:36:18 -0800

    Global retriever tested and it works, next enable defer message. Development environment test scripts fixed.
    - Create a first class TrainingSet model for representing training tasks.
    
    - Factorize LOPQ retriever into a "TrainedModel", "Retriever" and Indexer / "IndexEntry".
    
    - Start single machine YOLO training on AWS GPU instances.
    
    - Above factorization will make trained LOPQ retrievers portable across datasets.
    
    - Incorporate recently released YT8M trained indexer / retriever.

--- tests/clean.py
+++ tests/clean.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import django, os, sys, subprocess, shlex, shutil
+import django, os, sys, subprocess, shlex, shutil, glob
 sys.path.append('../server/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dva.settings")
 django.setup()
@@ -9,23 +9,27 @@ import utils
 
 if __name__ == '__main__':
     for qname in set(settings.TASK_NAMES_TO_QUEUE.values()):
-        subprocess.check_output(shlex.split('rabbitmqadmin purge queue name={}'.format(qname)))
+        try:
+            subprocess.check_output(shlex.split('rabbitmqadmin purge queue name={}'.format(qname)))
+        except:
+            print "error clearly {} passing".format(qname)
     # TODO: wait for Celery bug fix https://github.com/celery/celery/issues/3620
     # local('celery amqp exchange.delete broadcast_tasks')
     utils.migrate()
     subprocess.check_output(shlex.split('python manage.py flush --no-input'),cwd='../server')
     utils.migrate()
-    shutil.copy("../configs/custom_defaults/defaults_mac.py", 'dvaui/defaults.py')
+    shutil.copy("../configs/custom_defaults/defaults_mac.py", '../server/dvaui/defaults.py')
     for dirname in os.listdir(settings.MEDIA_ROOT):
-        if dirname != 'gitkeep':
-            shutil.rmtree("rm -rf {}/{}".format(settings.MEDIA_ROOT,dirname))
-    os.system("rm ../logs/*.log")
+        shutil.rmtree("{}/{}".format(settings.MEDIA_ROOT,dirname))
+    for log_filename in glob.glob("../logs/*.log"):
+        os.remove(log_filename)
     try:
-        os.system("ps auxww | grep 'celery -A dva' | awk '{print $2}' | xargs kill -9")
+        subprocess.check_output(['./kill.sh'])
     except:
         pass
-    os.environ['SUPERUSER'] = 'akshay'
-    os.environ['SUPERPASS'] = 'super'
-    os.environ['SUPEREMAIL'] = 'test@deepvideoanalytics.com'
-    subprocess.check_output(shlex.split('fab copy_defaults'),cwd='../server')
-    subprocess.check_output(shlex.split('fab init_fs'),cwd='../server')
+    envs = os.environ.copy()
+    envs['SUPERUSER'] = 'akshay'
+    envs['SUPERPASS'] = 'super'
+    envs['SUPEREMAIL'] = 'test@deepvideoanalytics.com'
+    subprocess.check_output(shlex.split('fab copy_defaults'),cwd='../server',env=envs)
+    subprocess.check_output(shlex.split('fab init_fs'),cwd='../server',env=envs)

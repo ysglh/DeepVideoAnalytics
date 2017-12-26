7fe487ff5e9584c7e606e9c4c63fe61c3e9138bd
akshay bhat <akshayubhat@gmail.com>
2017-12-26 13:46:05 -0800

    Add delays to kubernetes launch sequence to ensure that disks are allocated and database / rabbitmq  is up, before launching webserver.
    - Create a first class TrainingSet model for representing training tasks.
    
    - Factorize LOPQ retriever into a "TrainedModel", "Retriever" and Indexer / "IndexEntry".
    
    - Start single machine YOLO training on AWS GPU instances.
    
    - Above factorization will make trained LOPQ retrievers portable across datasets.
    
    - Incorporate recently released YT8M trained indexer / retriever.

--- deploy/gcp/launch.sh
+++ deploy/gcp/launch.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+set -xe
 kubectl create -f secrets.yml
 kubectl create -f deployments/gce-pd.yml
+kubectl create -f deployments/postgres.yaml
+kubectl create -f deployments/rabbitmq.yaml
+sleep 60
+kubectl create -f deployments/webserver.yaml
+sleep 30
 kubectl create -f deployments/coco.yaml
 kubectl create -f deployments/extractor.yaml
 kubectl create -f deployments/face.yaml
@@ -8,10 +14,7 @@ kubectl create -f deployments/facenet.yaml
 kubectl create -f deployments/facenet_retriever.yaml
 kubectl create -f deployments/inception.yaml
 kubectl create -f deployments/inception_retriever.yaml
-kubectl create -f deployments/postgres.yaml
-kubectl create -f deployments/rabbitmq.yaml
 kubectl create -f deployments/textbox.yaml
-kubectl create -f deployments/webserver.yaml
 kubectl create -f deployments/scheduler.yaml
 kubectl create -f deployments/crnn.yaml
 kubectl create -f deployments/tagger.yaml

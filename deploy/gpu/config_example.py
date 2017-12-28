2ca2cd38b82aed260837f89ef45ed22f2a4b0bfb
akshay bhat <akshayubhat@gmail.com>
2017-12-28 01:42:58 -0800

    Test run using AWS P2 instances.
    list.
    
    - Factorize LOPQ retriever into a "TrainedModel", "Retriever" and Indexer / "IndexEntry".
    
    - Start single machine YOLO training on AWS GPU instances.
    
    - Above factorization will make trained LOPQ retrievers portable across datasets.
    
    - Incorporate recently released YT8M trained indexer / retriever.

---
+++ deploy/gpu/config_example.py
@@ -0,0 +1,6 @@
+AMI = 'ami-37bb714d'
+env_user = "ubuntu"
+key_filename = "~/.ssh/<your_private_key>"
+KeyName = "key pair name from AWS"
+SecurityGroupName= "name of the security group"
+IAM_ROLE = {'Name':"IAM Role name that has S3 permissions, optional"}
--- deploy/gpu/launch.py
+++ deploy/gpu/launch.py
@@ -5,28 +5,21 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%m-%d %H:%M',
                     filename='../../logs/cloud.log',
                     filemode='a')
-env_user = "ubuntu"
-key_filename = ""
-AMI = ""
-KeyName = ""
-SecurityGroupId = ""
-IAM_ROLE = ""
+from config import AMI,KeyName,SecurityGroupName,IAM_ROLE,env_user,key_filename
 
 if __name__ == '__main__':
     ec2 = boto3.client('ec2')
     ec2r = boto3.resource('ec2')
     instances = ec2r.create_instances(DryRun=False, ImageId=AMI, KeyName=KeyName, MinCount=1, MaxCount=1,
-                                      SecurityGroups=[SecurityGroupId, ], InstanceType="p2.xlarge",
+                                      SecurityGroups=[SecurityGroupName, ], InstanceType="p2.xlarge",
                                       Monitoring={'Enabled': True, }, IamInstanceProfile=IAM_ROLE)
     for instance in instances:
         instance.wait_until_running()
         instance.reload()
         print(instance.id, instance.instance_type)
         logging.info("instance allocated")
-        with open("host", 'w') as out:
-            out.write(instance.public_ip_address)
         fh = open("connect.sh", 'w')
         fh.write(
-            "#!/bin/bash\n" + 'autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -L 8600:localhost:8000 -i ' + KeyName + " " + env_user + "@" +
+            "#!/bin/bash\n" + 'autossh -M 0 -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -L 8600:localhost:8000 -i ' + key_filename + " " + env_user + "@" +
             instance.public_ip_address + "\n")
         fh.close()

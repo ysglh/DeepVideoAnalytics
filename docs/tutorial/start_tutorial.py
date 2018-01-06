b354abdbf0b13944e910b14b2a9a03c132e08c4d
akshay bhat <akshayubhat@gmail.com>
2018-01-06 14:15:42 -0800

    Improve tutorial.
    - Start work on adding Redis for caching and implementing reduce using process watchers.
    
    - Implement training set creation / training.

---
+++ docs/tutorial/start_tutorial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os, requests, subprocess, time, webbrowser
+
+if __name__ == '__main__':
+    print "Checking if docker is running"
+    max_minutes = 20
+    try:
+        subprocess.check_call(["docker",'ps','-a'])
+    except:
+        raise SystemError("Docker is not running")
+    try:
+        subprocess.check_call(["docker-compose"])
+    except:
+        raise SystemError("Docker-compose is not available")
+    print "Trying to launch containers, first time it might take a while to download container images"
+    try:
+        compose_process = subprocess.Popen(["docker-compose",'up','-d'],
+                                           cwd=os.path.join(os.path.dirname(__file__),'../../deploy/cpu'))
+    except:
+        raise SystemError("Could not start container")
+    while max_minutes:
+        print "Waiting for {max_minutes} minutes, while checking if DVA is running".format(max_minutes=max_minutes)
+        try:
+            r = requests.get("http://localhost:8000")
+            if r.ok:
+                print "Open browser window and go to http://localhost:8000"
+                print 'For windows you might need to replace "localhost" with ip address of docker-machine '
+                webbrowser.open("http://localhost:8000")
+                webbrowser.open("http://localhost:8888")
+                break
+        except:
+            pass
+        time.sleep(60)
+        max_minutes -= 1
+    compose_process.wait()
--- server/launch_from_env.py
+++ server/launch_from_env.py
@@ -55,6 +55,8 @@ if __name__ == '__main__':
             else:
                 queue_name = k.split('_')[-1]
                 _ = subprocess.Popen(shlex.split(('./startq.py {}'.format(queue_name))))
+    if os.environ.get("LAUNCH_NOTEBOOK", False):
+        _ = subprocess.Popen(['./run_jupyter.sh','--allow-root'],cwd="/")
     if os.environ.get("LAUNCH_SCHEDULER", False):
         # Should be launched only once per deployment
         _ = subprocess.Popen(['./start_scheduler.py'])
